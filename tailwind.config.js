980c918e316c0fcef7647b33ab32865fc264e1de
IgorBekchruin <worki.iba@gmail.com>
2023-02-09 09:04:06 +0400

    tailblog without adaptive

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    screens:{
+      'lg': {'max': '992px'},
+      'md': {'max': '768px'},
+      'sm': {'max': '480px'},
+    },
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        black: '#232536',
+        yellow: '#FFD050',
+        purple: '#592EA9',
+        darkgrey: '#4C4C4C',
+        mdgrey: '#6D6E760',
+        lightgrey: '#F4F4F4',
+        lavender: '#F4F0F8',
+        lightyellow: '#FBF6EA',
+        light: '#FFFFFF',
+      }
+    },
+  },
+  plugins: [],
+}
